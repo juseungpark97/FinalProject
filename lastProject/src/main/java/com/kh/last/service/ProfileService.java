be5159b464ff27db4a7c283f19ca68667a45639b
Opyun0123 <omh656464@gmail.com>
2024-08-16 11:38:41 +0900

    프로필생성 해결
    수정 완료

--- lastProject/src/main/java/com/kh/last/service/ProfileService.java
+++ lastProject/src/main/java/com/kh/last/service/ProfileService.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.last.model.vo.Movie;
 import com.kh.last.model.vo.Profile;
