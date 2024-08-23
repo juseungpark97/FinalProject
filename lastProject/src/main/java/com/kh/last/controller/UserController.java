a4d83cb20108e0224f594e48e2ba87116b82f803
juseungpark97 <vmffpdlsdnjxj@gmail.com>
2024-08-23 14:58:07 +0900

    유저

--- lastProject/src/main/java/com/kh/last/controller/UserController.java
+++ lastProject/src/main/java/com/kh/last/controller/UserController.java
@@ -69,38 +69,38 @@ public class UserController {
 	}
 
 	// 사용자 로그인
-	@PostMapping("/login")
-	public ResponseEntity<LoginResponse> loginUser(@RequestBody UserLoginRequest request) {
-		try {
-			String status = userService.checkUserStatus(request.getEmail());
-		  if (status != null) {
-			  if (status.equals("S")) {
-				// 정지유저 경고 메시지 전달
-				EmailCheckResponse response = new EmailCheckResponse(false);
-				response.setMessage("정지된 계정입니다. 다른 계정으로 이용 부탁드립니다.");
-				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-			}
+	   @PostMapping("/login")
+	   public ResponseEntity<LoginResponse> loginUser(@RequestBody UserLoginRequest request) {
+	      try {
+	         String status = userService.checkUserStatus(request.getEmail());
+	         if (status != null) {
+	            if (status.equals("S")) {
+	               // 정지유저 경고 메시지 전달
+	               LoginResponse response = new LoginResponse(null);
+	               response.setMessage("정지된 계정입니다. 다른 계정으로 이용 부탁드립니다.");
+	               return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+	            }
+	            if (status.equals("D")) {
+	               // 탈퇴회원 안내 메시지 전달
+	               LoginResponse response = new LoginResponse(null);
+	               response.setMessage("탈퇴한 회원입니다.");
+	               return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+	            }
+	         }
 
-			if (status.equals("D")) {
-				// 탈퇴회원 안내 메시지 전달
-				EmailCheckResponse response = new EmailCheckResponse(false);
-				response.setMessage("탈퇴한 회원입니다.");
-				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-			}
-		}
-			String token = userService.loginUser(request.getEmail(), request.getPassword());
-			visitService.updateVisitCount();
+	         String token = userService.loginUser(request.getEmail(), request.getPassword());
+	         visitService.updateVisitCount();
 
-			// 구독 상태 확인
-			boolean isSubscribed = subscriptionService.isUserSubscribed(request.getEmail());
-			LoginResponse response = new LoginResponse(token);
-			response.setSubscribed(isSubscribed);
+	         // 구독 상태 확인
+	         boolean isSubscribed = subscriptionService.isUserSubscribed(request.getEmail());
+	         LoginResponse response = new LoginResponse(token);
+	         response.setSubscribed(isSubscribed);
 
-			return ResponseEntity.ok(response);
-		} catch (BadCredentialsException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-		}
-	}
+	         return ResponseEntity.ok(response);
+	      } catch (BadCredentialsException e) {
+	         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+	      }
+	   }
 
 	// 현재 로그인된 사용자 정보 가져오기
 	@PostMapping("/check-email")
